8375983a6485232dcbe29f4f7a3f00899f43e1a6
InoUno <inoiown@gmail.com>
2025-04-26 13:46:56 +0200

    Change registry order and simplify the code

--- src-tauri/src/ashita.rs
+++ src-tauri/src/ashita.rs
@@ -240,18 +240,14 @@ pub async fn update_gamepad_config(profile: &Profile) -> anyhow::Result<()> {
         )
     })?;
 
-    // Retrieve gamepad settings from registry. Try default first, then US, else EU.
+    // Retrieve gamepad settings from registry. Try US first, then JP, else EU.
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let mut ffxi_reg =
-        hklm.open_subkey("SOFTWARE\\WOW6432Node\\PlayOnline\\SQUARE\\FinalFantasyXI");
-    if ffxi_reg.is_err() {
-        ffxi_reg =
-            hklm.open_subkey("SOFTWARE\\WOW6432Node\\PlayOnlineUS\\SquareEnix\\FinalFantasyXI");
-        if ffxi_reg.is_err() {
-            ffxi_reg =
-                hklm.open_subkey("SOFTWARE\\WOW6432Node\\PlayOnlineEU\\SquareEnix\\FinalFantasyXI");
-        }
-    }
+    let ffxi_reg = hklm
+        .open_subkey("SOFTWARE\\WOW6432Node\\PlayOnlineUS\\SquareEnix\\FinalFantasyXI")
+        .or_else(|_| hklm.open_subkey("SOFTWARE\\WOW6432Node\\PlayOnline\\SQUARE\\FinalFantasyXI"))
+        .or_else(|_| {
+            hklm.open_subkey("SOFTWARE\\WOW6432Node\\PlayOnlineEU\\SquareEnix\\FinalFantasyXI")
+        });
 
     let Ok(ffxi_reg) = ffxi_reg else {
         tracing::warn!("Did not find FFXI registry to pull data from.");
